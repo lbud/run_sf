2613fc7121e953f7bd8d061ecec2990395ee8b71
lbud <lbudorick@gmail.com>
2014-03-14 17:46:14 -0700

    broken and uncommented because gah

--- build_db.py
+++ build_db.py
@@ -4,6 +4,7 @@ import save_data
 ## parse initial input XML
 
 file_to_parse = raw_input("file > ")
+data_types = raw_input("nodes or ways? > ")
 
 tree = ET.parse(file_to_parse)
 root = tree.getroot()
@@ -14,26 +15,94 @@ def check_amenity(node):
     else:
         return False
 
-node_list = root.findall('node')
+def is_highway(way):
+    return way.findall('./tag[@k="highway"]')
 
-amenity_count = 0
-node_count = 0
-accepted_nodes = 0
-
-for node in node_list:
-    node_count += 1
-    if not check_amenity(node):
-        accepted_nodes += 1
-        n_id = node.attrib.get('id')
-        n_lat = node.attrib.get('lat')
-        n_lon = node.attrib.get('lon')
-        # n_elev = node.attrib.get('elev')
-        n_elev = None
-        save_data.store_node(n_id, n_lat, n_lon, n_elev)
+def foot_restricted(way):
+    foot_rest = way.findall('./tag[@k="foot"][@v="no"]')
+    is_motorway = way.findall('./tag[@k="highway"][@v="motorway"]')
+    is_motorway_link = way.findall('./tag[@k="highway"][@v="motorway_link"]')
+    if foot_rest or is_motorway or is_motorway_link:
+        return True
     else:
-        amenity_count += 1
-save_data.session.commit()
+        return False
 
-print "amenities: ", amenity_count
-print "total nodes: ", node_count
-print "nodes added to db: ", accepted_nodes
+if data_types == "nodes":
+    node_list = root.findall('node')
+
+    amenity_count = 0
+    node_count = 0
+    accepted_nodes = 0
+
+    for node in node_list:
+        node_count += 1
+        if not check_amenity(node):
+            accepted_nodes += 1
+            n_id = node.attrib.get('id')
+            n_lat = node.attrib.get('lat')
+            n_lon = node.attrib.get('lon')
+            # n_elev = node.attrib.get('elev')
+            n_elev = None
+            save_data.store_node(n_id, n_lat, n_lon, n_elev)
+        else:
+            amenity_count += 1
+    save_data.session.commit()
+
+    print "amenities: ", amenity_count
+    print "total nodes: ", node_count
+    print "nodes added to db: ", accepted_nodes
+
+if data_types == "ways":
+
+    ## to get intersections
+    intersections = {}
+    for way in root.findall('way'):
+        if is_highway(way) and not foot_restricted(way):
+            for node in way.findall('./nd'):
+                n_ref = node.attrib.get('ref')
+                if not intersections.get(n_ref):
+                    intersections[n_ref] = 1
+                else:
+                    intersections[n_ref] += 1
+
+    valid_ints = []
+
+    for intersection in intersections.items():
+        if intersection[1] > 2:
+            valid_ints.append(intersection[0])
+            i_id = intersection[0]
+            i_ints = intersection[1]
+            ## find intersection location from nodes table
+            i_lat, i_lon = save_data.find_intersection(i_id)
+            i_elev = None
+            save_data.store_intersection(i_id, i_ints, i_lat, i_lon, i_elev)
+    save_data.session.commit()
+
+
+    ## to get all ways with nodes
+    ways = {}
+    for way in root.findall('way'):
+        if is_highway(way) and not foot_restricted(way):
+            w_id = way.attrib.get('id')
+            ways[w_id] = []
+            for node in way.findall('./nd'):
+                ways[w_id].append(node.attrib.get('ref'))
+
+
+    unfinished_edges = []
+    for way in ways.items():
+        this_edge = []
+        way_id = way[0]
+        intersections = way[1]
+        for i in range(len(intersections)):
+            if intersections[i] in valid_ints:
+                this_edge.append(intersections[i])
+                if len(this_edge) == 2:
+                    end_a = this_edge[0]
+                    end_b = this_edge[1]
+                    save_data.store_edge(way_id, end_a, end_b)
+                    this_edge = this_edge[1:]
+            if i == len(intersections) - 1 and not (intersections[i] in valid_ints):
+                this_edge.append(intersections[i])
+                unfinished_edges.append(this_edge)
+    save_data.session.commit()
--- save_data.py
+++ save_data.py
@@ -21,12 +21,24 @@ class Node(Base):
     lon = Column(Float)
     elev = Column(Float, nullable=True)
 
-# class Way(Base):
-#     __tablename__ = "ways"
+class Intersection(Base):
+    __tablename__ = "intersections"
+    id = Column(Integer, primary_key=True)
+    ints = Column(Integer)
+    lat = Column(Float, nullable=True)
+    lon = Column(Float, nullable=True)
+    elev = Column(Float, nullable=True)
 
-#     ## 
-#     ## finish defining way cols
-#     ##
+
+class Edge(Base):
+    __tablename__ = "edges"
+
+    id = Column(Integer, primary_key=True)
+    way_id = Column(Integer)
+    end_a = Column(Integer)
+    end_b = Column(Integer)
+
+
 
 def store_node(id, lat, lon, elev):
     n = Node(id=id, lat=lat, lon=lon, elev=elev)
@@ -34,6 +46,20 @@ def store_node(id, lat, lon, elev):
     ## not committing here so as to be able to add entire list at once
     return None
 
+def store_intersection(id, ints, lat, lon, elev):
+    i = Intersection(id=id, ints=ints, lat=lat, lon=lon, elev=elev)
+    session.add(i)
+    return None
 
-def store_ways():
-    pass
+def store_edge(way_id, end_a, end_b):
+    e = Edge(way_id=way_id, end_a=end_a, end_b=end_b)
+    session.add(e)
+    return None
+
+
+
+def find_intersection(id):
+    node = session.query(Node).get(id)
+    lat = node.lat
+    lon = node.lon
+    return lat, lon
